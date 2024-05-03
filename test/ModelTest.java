5a6740e890d544cc46245a95aa21acea53a1418b
Lucia <lposadagonzalez@danielcastelao.org>
2024-05-03 11:17:45 +0200

    Creado test - clase Model

--- test/ModelTest.java
+++ test/ModelTest.java
@@ -1,2 +1,16 @@
+
+import cod.mvc.Coche;
+import cod.mvc.Model;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ModelTest {
+
+    @Test
+    public void comprobacionCrearCoche() {
+        Coche coche = Model.crearCoche("43656C35FD","Seat",0);
+        Assertions.assertEquals("43656C35FD",coche.matricula);
+        Assertions.assertEquals("Seat",coche.modelo);
+        Assertions.assertEquals(0,coche.velocidad);
+    }
 }
