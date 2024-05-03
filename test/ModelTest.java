55bcb3cfe9375841b9daec6fbd9f4e7a37cdf957
Lucia <lposadagonzalez@danielcastelao.org>
2024-05-03 11:24:44 +0200

    Creado test comprobacionGetCoche()

--- test/ModelTest.java
+++ test/ModelTest.java
@@ -4,6 +4,8 @@ import cod.mvc.Model;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 public class ModelTest {
 
     @Test
@@ -12,5 +14,11 @@ public class ModelTest {
         Assertions.assertEquals("43656C35FD",coche.matricula);
         Assertions.assertEquals("Seat",coche.modelo);
         Assertions.assertEquals(0,coche.velocidad);
+    }
+
+    @Test
+    public void comprobacionGetCoche(){
+        Coche coche = Model.crearCoche("43656C35FD","Seat",0);
+        Assertions.assertSame(coche,Model.getCoche("43656C35FD"));
     }
 }
