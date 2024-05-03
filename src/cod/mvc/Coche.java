84bf571d1ffd8d5a95d4d0910456363ecff003cc
Lucia <lposadagonzalez@danielcastelao.org>
2024-05-03 09:45:01 +0200

    Creada clase coche

---
+++ src/cod/mvc/Coche.java
@@ -0,0 +1,15 @@
+
+package cod.mvc;
+
+public class Coche {
+
+    private String matricuka;
+    private String modelo;
+    private Integer velocidad;
+
+    public Coche(String matricuka, String modelo, Integer velocidad) {
+        this.matricuka = matricuka;
+        this.modelo = modelo;
+        this.velocidad = velocidad;
+    }
+}
--- src/cod/mvc/Main.java
+++ src/cod/mvc/Main.java
@@ -1,3 +1,4 @@
+
 package cod.mvc;
 
 public class Main {
