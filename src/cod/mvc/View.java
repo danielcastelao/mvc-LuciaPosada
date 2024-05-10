706d8e6dbd6fb07f1c83ebe01faebbc7c85170e8
Lucia <lposadagonzalez@danielcastelao.org>
2024-05-10 09:06:20 +0200

    Añadido java doc

--- src/cod/mvc/View.java
+++ src/cod/mvc/View.java
@@ -1,7 +1,11 @@
 package cod.mvc;
 
 public class View {
-
+    /**
+     * Muestra la velocidad actual del coche cuya matricula se le haya proporcionado
+     * @param matricula identificador unico del coche
+     * @param velocidad variable que se quiere mostrar
+     */
     public static void muestraVelocidad(String matricula,int velocidad){
         System.out.println("La velocidad de coche con matricula "+matricula+", es "+Model.getVelocidad(matricula));
     }
