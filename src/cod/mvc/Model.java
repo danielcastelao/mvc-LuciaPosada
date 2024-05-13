a5c9726cffecd555264ff4f270642c6c66085203
Lucia <lposadagonzalez@danielcastelao.org>
2024-05-13 08:14:06 +0200

    Añadido ArrayList de Observers

--- src/cod/mvc/Model.java
+++ src/cod/mvc/Model.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 
 public class Model {
 
+    public static ArrayList<Observer> observers = new ArrayList<>();
+
     public static ArrayList<Coche> parking = new ArrayList<>();
 
     /**
