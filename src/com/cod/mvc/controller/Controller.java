7223d1072a18c514bc57214425769d93a0591b09
Lucia <lposadagonzalez@danielcastelao.org>
2024-05-13 10:15:04 +0200

    Prueba controller

--- src/com/cod/mvc/controller/Controller.java
+++ src/com/cod/mvc/controller/Controller.java
@@ -1,6 +1,22 @@
 
 package com.cod.mvc.controller;
 
-public class Controller {
+import com.cod.mvc.model.Model;
 
+public class Controller {
+    static Model modelo;
+
+    public static void main(String[] args){
+        modelo = new Model();
+
+        ObserverVelocidad obVelocidad = new ObserverVelocidad();
+        modelo.addObserver(obVelocidad);
+
+        modelo.crearCoche("3546-DFS","Seat");
+        modelo.crearCoche("9775-LYD","Mini");
+
+        modelo.cambiarVelocidad("3546-DFS",36);
+        modelo.cambiarVelocidad("9775-LYD",28);
+
+    }
 }
