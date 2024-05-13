3c22372f9fa4d985b57c0a7b427ed9b99a8b0d45
Lucia <lposadagonzalez@danielcastelao.org>
2024-05-13 10:20:49 +0200

    Cambios concorde la modificacion del constructor de clase Coche + eliminado la instanciacion de la clase Model en el testeo del metodo cambiarVelocidad

--- test/ModelTest.java
+++ test/ModelTest.java
@@ -8,26 +8,25 @@ public class ModelTest {
 
     @Test
     public void comprobacionCrearCoche() {
-        Coche coche = Model.crearCoche("43656C35FD","Seat",0);
+        Coche coche = Model.crearCoche("43656C35FD","Seat");
         Assertions.assertSame(coche,Model.getCoche("43656C35FD"));
     }
 
     @Test
     public void comprobacionGetCoche(){
-        Coche coche = Model.crearCoche("43656C35FC","Seat",0);
+        Coche coche = Model.crearCoche("43656C35FC","Seat");
         Assertions.assertSame(coche,Model.getCoche("43656C35FC"));
     }
 
     @Test
     public void comprobacionCambiarVelocidad(){
-        Model model = new Model();
-        Coche coche = Model.crearCoche("43656C35FE","Seat",0);
-        Assertions.assertEquals(24,model.cambiarVelocidad("43656C35FE",24));
+        Coche coche = Model.crearCoche("43656C35FE","Seat");
+        Assertions.assertEquals(24,Model.cambiarVelocidad("43656C35FE",24));
     }
 
     @Test
     public void getVelocidad(){
-        Coche coche = Model.crearCoche("43656C35FR","Seat",45);
-        Assertions.assertEquals(45,Model.getVelocidad("43656C35FR"));
+        Coche coche = Model.crearCoche("43656C35FR","Seat");
+        Assertions.assertEquals(0,Model.getVelocidad("43656C35FR"));
     }
 }
