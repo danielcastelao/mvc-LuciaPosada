f7898547a63a9578253adc22a852a8bee0a6713c
Lucia <lposadagonzalez@danielcastelao.org>
2024-05-13 09:19:17 +0200

    Implementado el metodo update()

--- src/com/cod/mvc/controller/ObserverVelocidad.java
+++ src/com/cod/mvc/controller/ObserverVelocidad.java
@@ -1,10 +1,15 @@
 package com.cod.mvc.controller;
 
 import com.cod.mvc.model.Coche;
+import com.cod.mvc.view.View;
 
 public class ObserverVelocidad implements Observer{
+    /**
+     * Notifica a la clase view cuando se realiza un cambio de velocidad de un coche
+     * @param coche el objeto tipo coche que se a actualizado
+     */
     @Override
-    public void update(Coche arg) {
-
+    public void update(Coche coche) {
+        View.muestraVelocidad(coche.matricula,coche.velocidad);
     }
 }
