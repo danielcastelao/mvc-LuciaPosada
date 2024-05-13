4017d3a8263d5e74fe72c22fb69a7667adae1b07
Lucia <lposadagonzalez@danielcastelao.org>
2024-05-13 09:11:15 +0200

    Creada clase ObserverVelocidad

---
+++ src/com/cod/mvc/controller/ObserverVelocidad.java
@@ -0,0 +1,10 @@
+package com.cod.mvc.controller;
+
+import com.cod.mvc.model.Coche;
+
+public class ObserverVelocidad implements Observer{
+    @Override
+    public void update(Coche arg) {
+
+    }
+}
