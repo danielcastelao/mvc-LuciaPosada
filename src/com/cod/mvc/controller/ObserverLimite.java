114a64291ad9abc1d58ef36e1bfbb51090b33450
Lucia <lposadagonzalez@danielcastelao.org>
2024-05-20 08:51:12 +0200

    Creada constante para velocidad limite

--- src/com/cod/mvc/controller/ObserverLimite.java
+++ src/com/cod/mvc/controller/ObserverLimite.java
@@ -4,9 +4,11 @@ import com.cod.mvc.model.Coche;
 
 public class ObserverLimite implements Observer{
 
+    public final static int VELOCIDAD_LIMITE = 100;
+
     @Override
     public void update(Coche coche) {
-        if(coche.velocidad>100){
+        if(coche.velocidad>VELOCIDAD_LIMITE){
             System.out.println("Limite de velocidad sobrepasado");
         }
     }
