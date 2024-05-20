4b4120b19e8bf50e9f49476ae4b2d3249da26c0c
Lucia <lposadagonzalez@danielcastelao.org>
2024-05-20 09:33:09 +0200

    Prueba

--- src/com/cod/mvc/App.java
+++ src/com/cod/mvc/App.java
@@ -1,7 +1,19 @@
 package com.cod.mvc;
 
+import com.cod.mvc.controller.Controller;
+import com.cod.mvc.model.Model;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Model miModel = new Model();
+
+        Controller miController = new Controller(miModel);
+
+        miController.crearCoche("LaFerrari", "SBC 1234");
+        miController.crearCoche("Alpine", "HYU 4567");
+
+        miController.cambiarVelocidad("SBC 1234", 30);
+        miController.cambiarVelocidad("HYU 4567", 150);
+
     }
 }
