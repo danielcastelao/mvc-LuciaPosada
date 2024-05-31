ce6cccc3651b73c4c09ce97b7d9e9856cad8be5b
Lucia <lposadagonzalez@danielcastelao.org>
2024-05-31 09:19:04 +0200

    Creado metodo muestraDatosCoche

--- src/com/cod/mvc/view/View.java
+++ src/com/cod/mvc/view/View.java
@@ -11,4 +11,14 @@ public class View {
     public static void muestraVelocidad(String matricula,int velocidad){
         System.out.println("La velocidad del coche con matricula "+matricula+", es "+ velocidad+" Km/h");
     }
+
+    /**
+     * Moestra los datos del coche cuya matricula se le haya proporcionado
+     * @param matricula variable que se quiere mostrar
+     * @param model variable que se quiere mostrar
+     * @param velocidad variable que se quiere mostrar
+     */
+    public static void muestraDatosCoche(String matricula,String model,int velocidad){
+        System.out.println("Datos Coche:\nMatricula: "+matricula+"\nModelo: "+model+"\nVelocidad: "+velocidad);
+    }
 }
