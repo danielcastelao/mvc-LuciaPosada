31f51d6a77f1ecc0c2909fe9d19ae20a4bac1ee9
Lucia <lposadagonzalez@danielcastelao.org>
2024-05-31 09:21:01 +0200

    Añadido javaDoc faltante

--- src/com/cod/mvc/controller/ObserverBuscar.java
+++ src/com/cod/mvc/controller/ObserverBuscar.java
@@ -6,6 +6,10 @@ import com.cod.mvc.view.View;
 
 public class ObserverBuscar implements Observer{
 
+    /**
+     * Notifica a la clase view cuando se busca un coche
+     * @param coche el objeto tipo coche que se a actualizado
+     */
     @Override
     public void update(Coche coche, Model modelo) {
         View.muestraDatosCoche(coche.matricula,coche.modelo,coche.velocidad);
