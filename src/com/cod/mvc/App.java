6104165e0a408171959483e2fd405885a687a6aa
LuciaPosada <lposadagonzalez@danielcastelao.org>
2024-06-03 18:26:26 +0200

    Pruebando prorama

--- src/com/cod/mvc/App.java
+++ src/com/cod/mvc/App.java
@@ -11,9 +11,17 @@ public class App {
 
         miController.crearCoche("LaFerrari", "SBC 1234");
         miController.crearCoche("Alpine", "HYU 4567");
+        miController.crearCoche("Mini", "ITC 9937");
 
-        miController.cambiarVelocidad("SBC 1234", 30);
-        miController.cambiarVelocidad("HYU 4567", 150);
+        // Comprobacion subida
+        miController.subirVelocidad("ITC 9937", 70);
+        miController.subirVelocidad("SBC 1234", 30);
+
+        // Comprobacion bajada
+        miController.bajarVelocidad("ITC 9937", 50);
+
+        // Comporbacion sobrepaso del limite
+        miController.subirVelocidad("HYU 4567", 150);
 
     }
 }
